1b24c921d1074ccbde952343b42fb1f70a0287c1
Berliiyanto Aji <126399467+berlliyanto@users.noreply.github.com>
2024-02-04 20:53:42 +0700

    add awesome dialog package

---
+++ lib/app/components/widgets/dialog/awesome_dialog.dart
@@ -0,0 +1,21 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void awesomeDialog(
+    {required String title,
+    required String desc,
+    DialogType type = DialogType.info,
+    VoidCallback? callback}) {
+  AwesomeDialog(
+          context: Get.context!,
+          animType: AnimType.scale,
+          dialogType: type,
+          useRootNavigator: true,
+          title: title,
+          desc: desc,
+          btnOkOnPress: callback,
+          btnCancelOnPress: () => Get.back(),
+          showCloseIcon: true)
+      .show();
+}
