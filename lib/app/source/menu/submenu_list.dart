fa174e4ca11ce495958b362a67103782749c0b08
Berliiyanto Aji <126399467+berlliyanto@users.noreply.github.com>
2024-02-11 17:13:16 +0700

    update class name menu list

--- lib/app/source/menu/submenu_list.dart
+++ lib/app/source/menu/submenu_list.dart
@@ -1,46 +1,10 @@
 import 'package:of_flutter_mobile/app/routes/app_pages.dart';
 
-List<ListCheckUnitModel> listCheckUnit = [
-  ListCheckUnitModel(
-    title: "LIST FORKLIFT",
-    routes: Routes.LISTFORKLIFT,
-    image1: "assets/images/forklift.png",
-    image2: "assets/images/list-forklift.png",
-    permissions: "read-forklift",
-  ),
-  ListCheckUnitModel(
-    title: "ADD UNIT",
-    routes: Routes.ADDUNIT,
-    image1: "assets/images/forklift.png",
-    image2: "assets/images/add-unit.png",
-    permissions: "create-forklift",
-  ),
-  ListCheckUnitModel(
-    title: "CHECKLIST REPORT",
-    routes: Routes.CHECKREPORT,
-    image1: "assets/images/forklift.png",
-    image2: "assets/images/check-report.png",
-    permissions: "create-checklist",
-  ),
-  ListCheckUnitModel(
-    title: "LIST OPERATOR",
-    routes: Routes.LISTOPERATOR,
-    image1: "assets/images/list-operator.png",
-    permissions: "read-user",
-  ),
-  ListCheckUnitModel(
-    title: "CHECKLIST HISTORY",
-    routes: Routes.CHECKHISTORY,
-    image1: "assets/images/forklift.png",
-    permissions: "read-checklist",
-  ),
-];
-
-class ListCheckUnitModel {
+class SubMenuModel {
   final String title, routes, image1, image2;
   final String permissions;
 
-  ListCheckUnitModel({
+  SubMenuModel({
     required this.title,
     required this.routes,
     required this.image1,
@@ -48,3 +12,62 @@ class ListCheckUnitModel {
     this.permissions = "",
   });
 }
+
+//CHECK UNIT
+List<SubMenuModel> listCheckUnit = [
+  SubMenuModel(
+    title: "LIST FORKLIFT",
+    routes: Routes.LISTFORKLIFT,
+    image1: "assets/images/forklift.png",
+    image2: "assets/images/list-forklift.png",
+    permissions: "read-forklift",
+  ),
+  SubMenuModel(
+    title: "ADD UNIT",
+    routes: Routes.ADDUNIT,
+    image1: "assets/images/forklift.png",
+    image2: "assets/images/add-unit.png",
+    permissions: "create-forklift",
+  ),
+  SubMenuModel(
+    title: "CHECKLIST REPORT",
+    routes: Routes.CHECKREPORT,
+    image1: "assets/images/forklift.png",
+    image2: "assets/images/check-report.png",
+    permissions: "create-checklist",
+  ),
+  SubMenuModel(
+    title: "LIST OPERATOR",
+    routes: Routes.LISTOPERATOR,
+    image1: "assets/images/list-operator.png",
+    permissions: "read-user",
+  ),
+  SubMenuModel(
+    title: "CHECKLIST HISTORY",
+    routes: Routes.CHECKHISTORY,
+    image1: "assets/images/forklift.png",
+    permissions: "read-checklist",
+  ),
+];
+
+//HUMAN CAPITAL
+List<SubMenuModel> listHumanCapital = [
+  SubMenuModel(
+    title: "SLIP GAJI",
+    routes: "",
+    image1: "assets/images/slip-gaji.png",
+    permissions: "read-slip-gaji",
+  ),
+  SubMenuModel(
+    title: "PERMOHONAN ABSEN",
+    routes: "",
+    image1: "assets/images/absen.png",
+    permissions: "read-absen",
+  ),
+  SubMenuModel(
+    title: "PENILAIAN",
+    routes: "",
+    image1: "assets/images/penilaian.png",
+    permissions: "read-penilaian",
+  ),
+];
